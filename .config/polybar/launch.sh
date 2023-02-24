6b45d17ded11f697479031562eb01f8db4770d18
Niklas Modess <niklas@modess.io>
2023-02-24 10:04:11 +0100

    styling updates for polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use
-# polybar-msg cmd quit
 
-# Launch bar1 and bar2
-polybar top 2>&1 | tee -a /tmp/polybar-top.log & disown
-#polybar bottom 2>&1 | tee -a /tmp/polybar-bottom.log & disown
-
-echo "Bars launched..."
+polybar bottom 2>&1 | tee -a /tmp/polybar.log & disown
